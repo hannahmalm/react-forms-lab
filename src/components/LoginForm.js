7276f1945a0d1ec7ad60bad018e29acd233cac30
Hannah Malm <hannahemalm616@gmail.com>
2022-02-04 17:50:14 -0600

    commit tree 4b38f889e1c67508a306fed7c75e95944a2ce725

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,55 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  //have a hangle change and a handle submit event
+
+  //change the name to the value
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  // handleSubmit = event => {
+  //   event.preventDefault() //this is the same as in JS
+  //   if (this.state.username !== " " && this.state.password !== " ") {
+  //        this.props.handleLogin(this.state) 
+  //       }//handle login callback prop should be called 
+  // }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) return
+    this.props.handleLogin(this.state)
+  }
+
+/* <input
+id="username"
+type="text"
+name="username"
+value={this.state.username}
+onChange={this.handleInputChange}
+/> */
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,40 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
+  //change the message value on click
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  //This component takes one prop: maxChars which is a number — the maximum amount of characters a message can have.
+  //This prop is being passed in from the App component with the value 280.
+  //Its value should be saved in the component's state and should update on every change to the input.
+  //Show the remaining characters in the component. 
+  //It doesn't matter how you render it, as long as the number is correct. 
+
+  //Add the necessary event handler to the <form> element in order to call the onSubmit callback prop.
   render() {
+    let characterNumber = this.props.maxChars - this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        {characterNumber}
       </div>
-    );
+    )
   }
 }
+
+//saving input value in state - should update the state when typing
+//character counter - should show the remaining characters counter
+//should update the counter when typing
+
 
 export default TwitterMessage;
